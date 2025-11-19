cbc928e6cfb52b528f093bf3fc66457699a0667a
alsrl192 <alsrl192@gmail.com>
2025-11-19 09:11:20 +0900

    fix: bcryptjs -> bcrypt로 변경

--- api/update-admin-credentials.js
+++ api/update-admin-credentials.js
@@ -1,5 +1,5 @@
 const Database = require('better-sqlite3');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const path = require('path');
 
 const dbPath = path.join(__dirname, 'database.db');
